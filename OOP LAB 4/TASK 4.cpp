412b9bc84466a3be1de9338f10caba60fc4a11e0
Asad Ahmed <asadahmedk09@gmail.com>
2025-02-16 12:24:39 +0500

    Create TASK 4.cpp

---
+++ OOP LAB 4/TASK 4.cpp
@@ -0,0 +1,125 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+class Book
+{
+private:
+  string title;
+  float price;
+  int *stock = new int;
+  float totalPrice;
+  float discount;
+public:
+  Book(float p,string title,int s)
+  {
+    this->title = title;
+    price = p;
+    *stock = s;
+    totalPrice+=price*(*stock);
+    discount = 0;
+  }
+  Book()
+  {
+    title = "Unknown";
+    price = 0.0;
+    *stock = 30;
+    totalPrice = price;
+  }
+  float applyDiscount(int purchaseItem)
+  {
+    float newprice;float discount;
+    if(purchaseItem > 5 && purchaseItem <= 10)
+    {
+      discount = (price*purchaseItem)*0.05;
+      return discount;
+    }
+    else if(purchaseItem > 10)
+    {
+      discount = (price*purchaseItem)*0.10;
+      return discount;
+    }
+    return 0;
+  }
+  void UpdateBookDetails()
+  {
+    cout << "Enter the title of Book : " << endl;
+    getline(cin, title);
+    cout << "Enter the price of Book : " << endl;
+    cin >> price;
+    cout << "Enter the Item to be added in stock : " << endl;
+    int add;
+    cin >> add;
+    *stock += add;
+    totalPrice+=(price*(*stock));
+    cout << "Updated successfull and The total price for Added " << add << " and already stocked "<< (*stock-add)<< " BOOks is : " << totalPrice
+    << endl; 
+  }
+  void simulateBookPurchase()
+  {
+    cout << "Enter the number of Books you want to Purchase " << endl;
+    int no;
+    cin >> no;
+    float disc = applyDiscount(no);
+    if (*stock < no)
+    {
+      cout << "Sorry ! you do not have Enough inventory\nThe remaining stock is "<< *stock << endl;
+    }
+    else if (*stock < 5)
+    {
+      cout << "Low stock warning ! \nPurchase successfull!" << endl;
+      *stock -= no;
+      float finalPrice = (price * no) - disc;
+      totalPrice -= finalPrice;
+    }
+    else
+    {
+      float finalPrice = (price * no)- disc;
+      cout << "Purchase Successfull !" << endl
+           << "At the price of " << price * no << endl
+           << "And Discount is " << disc << endl
+           << "SO final price is " <<finalPrice << endl
+           << "of " << no << " Books " << endl;
+      *stock -= no;
+      totalPrice -= finalPrice;
+    }
+  }
+  void TrackStock()
+  {
+    cout << "The stock is " << *stock << endl;
+    cout << "The price of stock is " << totalPrice << endl;
+  }
+  void PriceCalculationMechanism()
+  {
+    float price = this->price;
+    int quantity;
+    cout <<"Enter the quantity of Purchase" << endl;
+    cin >> quantity;
+    float discount =applyDiscount(quantity);
+    cout << "your discount would be " << discount << " if you buy " << quantity << " books" << endl
+    << " at the rate of " << (price*quantity)-discount << endl;
+
+  }
+  Book (Book &b)
+  {
+    title = b.title;
+    price = b.price;
+    totalPrice = b.totalPrice;
+    stock = new int;
+    *stock = *(b.stock);
+  }
+  ~Book()
+  {
+    cout << "The object is being destroyed" << endl;
+    delete stock;
+  }
+};
+int main()
+{
+  Book b1;
+  b1.UpdateBookDetails();
+  Book b2 = b1;
+  b1.simulateBookPurchase();
+  b2.PriceCalculationMechanism();
+  b1.TrackStock();
+  b2.TrackStock();
+}
