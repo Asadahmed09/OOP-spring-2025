15cf428341c4ec26eac5ddbdec8e333937b74ebf
Asad Ahmed <asadahmedk09@gmail.com>
2025-04-11 21:39:22 +0500

    Create TASK 2.cpp

---
+++ OOP LAB 8/TASK 2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Book 
+{
+private:
+    string title;
+    double price;
+
+public:
+    Book(string t, double p) : title(t), price(p) {}
+
+    friend class Librarian;
+};
+
+class Librarian
+ {
+public:
+    void displayBookDetails(Book& book) 
+{
+        cout << "Title: " << book.title << ", Price: $" << book.price << endl;
+    }
+
+    void applyDiscount(Book& book, double discountPercent)
+ {
+        book.price *= (1 - discountPercent / 100);
+    }
+};
+
+int main()
+ {
+    Book book("The C++ ", 45.99);
+    Librarian lib;
+    
+    lib.displayBookDetails(book);
+    lib.applyDiscount(book, 10);
+    lib.displayBookDetails(book);
+}
